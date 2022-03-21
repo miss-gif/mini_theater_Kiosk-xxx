bd31a0982f478e28dd596cce64f1b7c84ffc48ae
miss-gif <ddc.akari@gmail.com>
2022-03-21 18:27:19 +0900

    JS FIX

--- JS/windowOnload.js
+++ JS/windowOnload.js
@@ -1,7 +1,5 @@
 // 페이지가 로드되었을때 실행
 
-window.addEventListener('load',function(){
-  //showStoreSelectPage();
-  hideAllPages();
-  showMovieSelectPage();
+window.addEventListener("load", function () {
+  showStoreSelectPage();
 });
